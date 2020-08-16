83f243b2d5a2e16b357733b99937f1629f48255f
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-08-16 14:02:54 +0200

    Add example code for enum initialization.

--- code/main.c
+++ code/main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum compliantEnum_1
+{
+    C1_E1,
+    C1_E2,
+    C1_E3
+};
+
+enum compliantEnum_2
+{
+    C2_E1 = 1,
+    C2_E2,
+    C2_E3
+};
+
+enum compliantEnum_3
+{
+    C3_E1 = 1,
+    C3_E2 = 3,
+    C3_E3 = 4
+};
+
+enum nonCompliantEnum
+{
+    NC_E1 = 1,
+    NC_E2,
+    NC_E3 = 2
+};
+
 int main(void)
 {
     printf("Hello world!");
