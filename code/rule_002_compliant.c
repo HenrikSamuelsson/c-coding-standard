4fd07f3c9c1bf3860df1fc09f2c6bd77aab4b01b
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-09-05 21:42:34 +0200

    Source code with compliant way to creata an enum

---
+++ code/rule_002_compliant.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+typedef enum eCompassBearing
+{
+	NORTH,
+	EAST,
+	SOUTH,
+	WEST
+} compassBearing;
+
+typedef enum eDirection
+{
+	UP,
+	RIGHT,
+	DOWN,
+	LEFT
+} direction;
+
+int f(direction d);
+
+int main(void)
+{
+	compassBearing cB = UP;
+	if (cB == UP)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+int f(direction d)
+{
+	if (d != UP)
+	{
+		printf("The only way is up!");
+	}
+
+	return 0;
+}
