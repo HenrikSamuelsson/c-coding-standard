afbadb819bdaeb0f535e1a6a000d1e29a230bd30
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-09-12 22:50:45 +0200

    Change font for the text.

---
+++ code/doxy-rule-001.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* Compliant way to mark a doxygen comment block below */
+
+/**
+ *
+ */
+
+/* Examples of non-compliant ways to mark doxygen comment blocks below */
+
+/*!
+ *
+ */
+
+///
+///
+///
+
+int main (void)
+{
+    printf("Doxygen rule 001");
+}
