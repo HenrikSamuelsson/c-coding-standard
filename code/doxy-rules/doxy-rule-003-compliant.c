d3dd9d44a7b2c24b6c4775614a7c9ac456388948
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-09-19 20:52:57 +0200

    Add compliant example of details usage.

---
+++ code/doxy-rules/doxy-rule-003-compliant.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
+
+/* Compliant way to use \details in doxygen comment block below */
+
+/**
+ * \brief   Get voltage level.
+ *
+ * \details Returns the battery
+ *          voltage level.
+ *
+ * \details Voltage is represented
+ *          as an int in mV. For
+ *          example 3.3 V will be
+ *          returned as 3300.
+ *
+ * \param   Void.
+ * \return  The voltage level.
+ */
+int getVoltage(void);
+
+int main(void)
+{
+    printf("Doxygen rule 003 .");
+}
+
+int getVoltage(void)
+{
+    return 3300;    /* 3.3 V */
+}
