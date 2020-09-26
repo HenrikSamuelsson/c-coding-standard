8de3e56e52427e0549b28a6c86542d2f5da9df6e
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-09-26 08:03:51 +0200

    Add compilant code example for doxygen rule 6.

--- code/doxy-rules/doxy-rule-006-compliant.c
+++ code/doxy-rules/doxy-rule-006-compliant.c
@@ -1,20 +1,22 @@
 #include <stddef.h>
 /**
- * \brief Copies a given number of 
- *        bytes.
- * 
- * \details where both areas may not overlap.
- * 
- * \param[out] dest Memory area to 
- *                  copy to.
- * \param[in]  src  Memory area to 
- *                  copy from.
- * \param[in]  n    Number of 
- *                  bytes to copy.
- * \return     Void. 
+ * \brief      Copies RAM data.
+ *
+ * \details    Copies a given number
+ *             af bytes from a source
+ *             memory area to a
+ *             destination memory
+ *             area, where areas may
+ *             not overlap.
+ *
+ * \param[out] dest Destination area.
+ * \param[in]  src  Source area.
+ * \param[in]  n    Number of bytes.
+ *
+ * \return     Void.
  */
-void memcpy(void *dest, 
-            const void *src, 
+void memcpy(void *dest,
+            const void *src,
             size_t n);
 
 int main(void)
