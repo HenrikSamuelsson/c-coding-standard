93b17f6e2aa443a4ed0559f1ebea3c7cdd59e4ce
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-09-26 20:32:08 +0200

    Add new source code that will serve as base for documentation.

---
+++ code/doxy-rules/doxy-rule-007-compliant.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * \brief      Determines the largest
+ *             of two float numbers.
+ *
+ * \param[in]  x First float number.
+ * \param[in]  y Second float number.
+ *
+ * \return     The largest of the two
+ *             provided float numbers.
+ */
+float fmaxf(float x, float y);
+
+int main(void)
+{
+    printf("Non compliant way of "
+           "documenting function "
+           "return./n");
+
+    return EXIT_SUCCESS;
+}
---
+++ code/doxy-rules/doxy-rule-007-non-compliant.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * \brief      Determines the largest
+ *             of two float numbers.
+ *
+ * \details    Returns the largest of
+ *             the two provided float
+ *             numbers.
+ *
+ * \param[in]  x First float number.
+ * \param[in]  y Second float number.
+ */
+float fmaxf(float x, float y);
+
+int main(void)
+{
+    printf("Non compliant way of "
+           "documenting function "
+           "return./n");
+
+    return EXIT_SUCCESS;
+}
