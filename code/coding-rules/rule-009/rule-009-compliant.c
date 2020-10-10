9891cb2c05ae3cb647aa64db606a271308c54318
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-10-10 17:48:18 +0200

    Add rule discussing how to comment out code.

---
+++ code/coding-rules/rule-009/rule-009-compliant.c
@@ -0,0 +1,14 @@
+/**
+ * \file rule-009-compliant.c
+ */
+#include <stdlib.h>
+
+int main(void)
+{
+#if 0
+    printf("Now in main().\n");
+#endif
+    printf("Hello world!\n");
+
+    return EXIT_SUCCESS;
+}
