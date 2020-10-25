e55a5c7eca83e183fea2ec100f6d89fb1bd1338e
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-10-25 08:39:38 +0100

    Add doxygen comments to main.c.

--- code/LedControl/src/main.c
+++ code/LedControl/src/main.c
@@ -1,11 +1,26 @@
+/*!
+ * \file main.c
+ */
 #include "setup.h"
 #include "controller.h"
 
-
-
-#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
+/*!
+ * \brief      Main function that does setup and runs the controller.
+ *
+ * \details    Main entry point of the program. Will first setup the system, and
+ *             from then on invoke the controller task function repeatedly as
+ *             fast as possible.
+ *
+ *             This function is intentionally designed to never return.
+ *
+ * \param      Void.
+ *
+ * \return     Status code, but will in reality never return.
+ */
 int main(void)
 {
     setup();
@@ -15,6 +30,7 @@ int main(void)
         controllerTask();
     }
 
+    assert(false);    /* This and below lines should never be reached. */
+
     return EXIT_SUCCESS;
 }
-
