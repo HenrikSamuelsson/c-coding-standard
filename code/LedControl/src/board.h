df1bd83302af024bbd11da40df2817920c10d303
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-11-07 16:51:32 +0100

    Move code for clock enable and switch button input to dedicated functions.

--- code/LedControl/src/board.h
+++ code/LedControl/src/board.h
@@ -8,16 +8,19 @@
 #ifndef BOARD_H_
 #define BOARD_H_
 
-/** Port D, bit 15 output to red LED. */
+/** Port D pin 15 used as output to control red LED. */
 #define PTD15    15
 
-/** Port D, bit 16, output to green LED. */
+/** Port D bit 16 used as output to control green LED. */
 #define PTD16    16
 
-/** Port D, bit 0, output to blue LED. */
+/** Port D pin 0 used as output to control blue LED. */
 #define PTD0     0
 
-/** Port C, bit 12, input from push button SW2. */
+/** Port C pin 12 used as input from push button SW2. */
 #define PTC12    12
+
+/** Port C pin 13 used as input from push button SW1. */
+#define PTC13    13
 
 #endif /* BOARD_H_ */
--- code/LedControl/src/setup.c
+++ code/LedControl/src/setup.c
@@ -4,17 +4,57 @@
 
 #include "S32K144.h"
 
+/**
+ * \brief      Enables the clock for all used port peripherals.
+ *
+ * \param      Void.
+ *
+ * \return     Void.
+ */
+static void enablePortClock(void);
+
+/**
+ * \brief      Configure input pins for switch buttons.
+ *
+ * \details    The pins connected to switch buttons are setup as:
+ *             - Input
+ *             - GPIO mode
+ *             - Passive input filter enabled
+ *
+ * \param      Void.
+ *
+ * return      Void.
+ */
+void setupSwitchButtonInputs(void);
+
 void setup(void)
 {
-    /* Enable clocks to PORT peripherals. */
-    PCC->PCCn[PCC_PORTC_INDEX] = PCC_PCCn_CGC_MASK; /* Enable clock to PORT C. */
-    PCC->PCCn[PCC_PORTD_INDEX] = PCC_PCCn_CGC_MASK; /* Enable clock to PORT D. */
-
-    /* Configure port C12 as GPIO input (BTN 0 [SW2] on EVB) */
-    PTC->PDDR &= ~(1 << PTC12); /* Port C12: Data Direction= input (default) */
-    PORTC->PCR[12] = PORT_PCR_MUX(1) | PORT_PCR_PFE_MASK; /* Port C12: MUX = GPIO, input filter enabled */
+    enablePortClock();
+    setupSwitchButtonInputs();
 
     /* Configure port D0 as GPIO output (LED on EVB) */
     PTD->PDDR |= 1 << PTD0; /* Port D0: Data Direction= output */
     PORTD->PCR[0] = PORT_PCR_MUX(1); /* Port D0: MUX = GPIO */
+}
+
+static void enablePortClock(void)
+{
+    /* Enable the clock to PORT C. */
+    PCC->PCCn[PCC_PORTC_INDEX] = PCC_PCCn_CGC_MASK;
+
+    /* Enable the clock to PORT D. */
+    PCC->PCCn[PCC_PORTD_INDEX] = PCC_PCCn_CGC_MASK;
+}
+
+void setupSwitchButtonInputs(void)
+{
+    /* Setup of switch button SW1 that connects pin PTC13. */
+    PTC->PDDR &= ~(1 << PTC13);              /* Set pin as input. */
+    PORTC->PCR[PTC13] |= PORT_PCR_MUX(1);    /* Set alternative 1 (GPIO). */
+    PORTC->PCR[PTC13] |= PORT_PCR_PFE_MASK;  /* Input filter enabled. */
+
+    /* Setup of switch button SW2 that connects pin PTC12. */
+    PTC->PDDR &= ~(1 << PTC12);              /* Set pin as input. */
+    PORTC->PCR[PTC12] |= PORT_PCR_MUX(1);    /* Set alternative 1 (GPIO). */
+    PORTC->PCR[PTC12] |= PORT_PCR_PFE_MASK;  /* Input filter enabled. */
 }
