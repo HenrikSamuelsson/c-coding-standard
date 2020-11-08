a869a91026767762d0c708e6acd4287ff674b54d
ShinyGreenRobot <henrik.samuelsson@gmail.com>
2020-11-08 08:10:21 +0100

    Add handling of the case that the button SW1 is pressed.

--- code/LedControl/src/switch_button.c
+++ code/LedControl/src/switch_button.c
@@ -8,17 +8,36 @@
 
 #include "S32K144.h"
 
+#include <assert.h>
+
 bool SWBTN_isEnabled(SwitchButtonType switchButton)
 {
     bool result;
 
-    if (PTC->PDIR & (1 << SW2_PIN))
+    switch (switchButton)
     {
-        result = true;
-    }
-    else
-    {
-        result = false;
+    case SW1:
+        if (PTC->PDIR & (1 << SW1_PIN))
+        {
+            result = true;
+        }
+        else
+        {
+            result = false;
+        }
+        break;
+    case SW2:
+        if (PTC->PDIR & (1 << SW2_PIN))
+        {
+            result = true;
+        }
+        else
+        {
+            result = false;
+        }
+        break;
+    default:
+        assert(false); /* Unknown switch button, should never happen. */
     }
 
     return result;
